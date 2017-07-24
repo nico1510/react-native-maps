b66fef88fe19866b5c85399f0e76ea011b13975c
Nicolas Beck <Nicolas-Beck@gmx.de>
2017-07-24 08:09:48 +0200

    fixed missing conversion from radians -> decimal

--- lib/ios/AirGoogleMaps/GMSCameraPositionUtils.m
+++ lib/ios/AirGoogleMaps/GMSCameraPositionUtils.m
@@ -27,9 +27,9 @@
     CGFloat y = (cos(neLatitude) * sin(neLongitude)) + (cos(swLatitude) * sin(swLongitude)) / 2;
     CGFloat z = (sin(neLatitude) + sin(swLatitude)) / 2;
 
-    CGFloat centralLongitude = atan2(y, x);
     CGFloat centralSquareRoot = sqrt(x * x + y * y);
-    CGFloat centralLatitude = atan2(z, centralSquareRoot);
+    CGFloat centralLatitude = atan2(z, centralSquareRoot) * 180 / M_PI;
+    CGFloat centralLongitude = atan2(y, x) * 180 / M_PI;
 
     return CLLocationCoordinate2DMake(centralLatitude, centralLongitude);
 
